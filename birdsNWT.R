3547c95e139383434e4911893149f1392d0cdce7
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2026-03-11 20:24:49 -0700

    add missing pkgs

--- birdsNWT.R
+++ birdsNWT.R
@@ -18,7 +18,7 @@ defineModule(sim, list(
   citation = list("citation.bib"),
   documentation = list("README.txt", "birdsNWT.Rmd"),
   reqdPkgs = list("googledrive", "magrittr", "data.table", "gbm",
-                  "tati-micheletti/usefulFuns@development", ## TODO: add pemisc for cluster fns
+                  "SpaDES.core", "reproducible", "terra",
                   "future", "future.apply", "tictoc"), # "raster", "plyr", "dplyr", "crayon",
   parameters = rbind(
     defineParameter("scenario", "character", NA, NA, NA,
