47de99fd93779349d0dcfa332a7a438f2a3960dc
CeresBarros <ceres.barros@nrcan-rncan.gc.ca>
2026-03-24 17:27:14 -0700

    make downloadBirdModels work

--- R/downloadBirdModels.R
+++ R/downloadBirdModels.R
@@ -1,16 +1,24 @@
-downloadBirdModels <- function(folderUrl, 
-                               version, 
-                               birdsList, 
+downloadBirdModels <- function(folderUrl,
+                               version,
+                               birdsList,
                                modelsPath,
                                returnPath = FALSE){
-  
-  filesToDownload <- googledrive::drive_ls(path = as_id(folderUrl), 
-                           pattern = paste0("brt", version, ".R"))
-  
-  modelsForBirdList <- filesToDownload$name[grepl(pattern = paste(birdsList, collapse = "|"), 
+  filesToDownload <- tryCatch({
+    drive_ls(path = as_id(folderUrl),
+                          pattern = paste0("brt", version, ".R")) |>
+      Cache()
+  }, error = function(e) {
+    ## Ceres: work around for sim$urlModels in shared drive
+    drive_ls(path = as_id(folderUrl),
+                         pattern = paste0("brt", version, ".R"), ## to make sure only projection file names are listed
+                         shared_drive = as_shared_drive(as_id("0ABSERKnd1o3sUk9PVA"))) |>
+      Cache()
+  })
+
+  modelsForBirdList <- filesToDownload$name[grepl(pattern = paste(birdsList, collapse = "|"),
                                                   x = filesToDownload$name)]
   if (length(modelsForBirdList) == 0){
-    message(crayon::red(paste0("No model available for ", birdsList, 
+    message(crayon::red(paste0("No model available for ", birdsList,
                    " for models V", version)))
     return(NA)
   }
@@ -22,10 +30,10 @@ downloadBirdModels <- function(folderUrl,
     if (returnPath){
       return(file.path(modelsPath, modelFile))
     } else {
-      return(get(load(file.path(modelsPath, modelFile)))) 
+      return(get(load(file.path(modelsPath, modelFile))))
     }
   })
   names(downloadedModels) <- birdsList
-  
+
   return(downloadedModels)
 }
